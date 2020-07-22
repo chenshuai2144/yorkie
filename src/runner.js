4e1e44a8ce0785c44626e832d3ff0473d5f5c6c6
chenshuai2144 <qixian.cs@outlook.com>
2020-07-22 19:40:07 +0800

    default set pre-commit

--- src/runner.js
+++ src/runner.js
@@ -1,23 +1,23 @@
-const fs = require('fs')
-const path = require('path')
-const execa = require('execa')
+const fs = require('fs');
+const path = require('path');
+const execa = require('execa');
 
-const cwd = process.cwd()
-const pkg = fs.readFileSync(path.join(cwd, 'package.json'))
-const hooks = JSON.parse(pkg).gitHooks
+const cwd = process.cwd();
+const pkg = fs.readFileSync(path.join(cwd, 'package.json'));
+const hooks = { 'pre-commit': 'npm run precommit', ...JSON.parse(pkg).gitHooks };
 if (!hooks) {
-  process.exit(0)
+  process.exit(0);
 }
 
-const hook = process.argv[2]
-const command = hooks[hook]
+const hook = process.argv[2];
+const command = hooks[hook];
 if (!command) {
-  process.exit(0)
+  process.exit(0);
 }
 
-console.log(` > running ${hook} hook: ${command}`)
+console.log(` > running ${hook} hook: ${command}`);
 try {
-  execa.shellSync(command, { stdio: 'inherit' })
+  execa.shellSync(command, { stdio: 'inherit' });
 } catch (e) {
-  process.exit(1)
+  process.exit(1);
 }
